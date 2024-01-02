a81f9d65ab3e0a10b55caf0abcd4e90c9fffe50f
Shakelvia Braden <117491124+thetechnicalnurse@users.noreply.github.com>
2024-01-02 01:32:31 -0600

    Created redux store with query to fetch all books.

--- src/store.js
+++ src/store.js
@@ -1,7 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const booksApi = createApi({
+    reducerPath: 'booksApi',
+    baseQuery: fetchBaseQuery({baseUrl: 'https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api'}),
+    tagTypes: ['Books'],
+    endpoints: (builder) => ({
+        getAllBooks: builder.query({
+            query: () => '/books',
+            providesTags: 'Books'
+        })
+    })
+})
 
 export const store = configureStore({
     reducer: {
-
-    }
+        booksApi: booksApi.reducer
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(booksApi.middleware)
 })
+
+export const { useGetAllBooksQuery } = booksApi
