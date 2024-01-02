9bbc020875d79b5d4ee25b7a01b4ced2c6764060
Shakelvia Braden <117491124+thetechnicalnurse@users.noreply.github.com>
2024-01-02 01:34:30 -0600

    Render and style books page. Add navigation to button.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import bookLogo from './assets/books.png'
 import Books from './components/Books'
 import { Route, Routes } from 'react-router-dom'
 import { NavBar } from './components/Navigations'
+import { SingleBook } from './components/SingleBook'
 
 function App() {
   const [token, setToken] = useState(null)
@@ -15,7 +16,7 @@ function App() {
         <Route path='/login' element={<h1>Login</h1>} />
         <Route path='/register' element={<h1>Register</h1>} />
         <Route path='/account' element={<h1>account</h1>} />
-        <Route path='/book/:id' element={<h1>book/:id</h1>} />
+        <Route path='/book/:id' element={<SingleBook />} />
       </Routes>
     </>
   )
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,67 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
+import React from "react"
+import { useNavigate } from "react-router-dom";
+import { useGetAllBooksQuery } from '../store'
+import {
+	Grid,
+	Box,
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Button,
+	Typography,
+	CircularProgress
+} from '@mui/material';
+
 
 const Books = () => {
-    return (
-        <h1>Books</h1>
-    )
+	const { data: books, isLoading, error } = useGetAllBooksQuery()
+	const navigate = useNavigate()
+
+	return (
+		<>
+			<Typography sx={{ textAlign: 'center', fontSize: 45 }}>Books</Typography>
+			{isLoading ?
+				<Box sx={{ display: 'flex', alignItems: 'center' }}>
+					<CircularProgress />
+				</Box> :
+				<Grid container spacing={3} padding={5}>
+					{books ?
+						books.books.map((book) => {
+							return (
+								<Box key={book.id} sx={{ margin: 1 }}>
+									<Card variant="outlined" sx={{ height: 425, width: 300, border: '5px solid PaleTurquoise' }}>
+										<Box sx={{ padding: 1 }}>
+											<CardContent sx={{ textAlign: 'center', paddingBottom: 0 }}>
+												<Typography variant="h6" component="div">
+													{book.title}
+												</Typography>
+												<Typography color="text.secondary">
+													{`Author: ${book.author}`}
+												</Typography>
+											</CardContent>
+											<CardMedia
+												sx={{ height: 300, width: 200, margin: 'auto' }}
+												image={book.coverimage}
+												title={book.title}
+											/>
+											<CardActions>
+												<Button size="small" sx={{ margin: 'auto' }} onClick={() => navigate(`/book/
+												${book.id}`)}>Details</Button>
+											</CardActions>
+										</Box>
+									</Card>
+								</Box>
+							)
+						})
+						: null}
+				</Grid>
+			}
+		</>
+
+
+	)
 }
 
 export default Books
