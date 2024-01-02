e5281893aac21f5b864a2d713b6a0a267d9bc5a9
Shakelvia Braden <117491124+thetechnicalnurse@users.noreply.github.com>
2024-01-02 03:55:54 -0600

    Style navigation bar. Add routing to auth form.

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,16 +1,41 @@
-/* TODO - add your code to create a functional React component that renders a navigation bar for the different views in your single page application. You may consider conditionally rendering some options - for example 'Login' should be available if someone has not logged in yet. */ 
+/* TODO - add your code to create a functional React component that renders a navigation bar for the different views in your single page application. You may consider conditionally rendering some options - for example 'Login' should be available if someone has not logged in yet. */
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 //Assests
 import bookLogo from '../assets/books.png'
 
+//Material UI
+import {
+	Box,
+	AppBar,
+	Toolbar,
+	Typography,
+	Button
+} from '@mui/material'
+
 export const NavBar = () => {
-    return (
-        <div>
-            <h1><img id='logo-image' src={bookLogo} />Library App</h1>
-            <Link to='/login'>Login</Link>
-            <Link to='/register'>Register</Link>
-        </div>
-    )
+	const navigate = useNavigate()
+	return (
+		<>
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position="static" sx={{backgroundColor: 'black'}}>
+					<Toolbar sx={{ fontFamily: 'Georgia' }}>
+							<Typography variant="h6" component="div" sx={{ flexGrow: 1 }} onClick={() => navigate('/')}>
+								<Button>
+									<Typography sx={{color: 'white', fontFamily: 'Georgia', fontSize: 30, textTransform: 'none'}}>
+									<img id='logo-image' src={bookLogo} /> 
+									Library App
+									</Typography>
+								</Button>
+							</Typography>
+						<Button color="inherit" onClick={() => navigate('/login')}>Login</Button>
+						<Button color="inherit" onClick={() => navigate('/register')}>Register</Button>
+					</Toolbar>
+				</AppBar>
+			</Box>
+		</>
+
+	)
 }
