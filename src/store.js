6c8d6098bf132e2ec9b5bb6ba295edb3d9d11bbb
Shakelvia Braden <117491124+thetechnicalnurse@users.noreply.github.com>
2024-01-09 11:25:39 -0600

    Add query mutations for login/register.

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const booksApi = createApi({
     reducerPath: 'booksApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api'}),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api' }),
     tagTypes: ['Books'],
     endpoints: (builder) => ({
         getAllBooks: builder.query({
@@ -17,12 +17,57 @@ const booksApi = createApi({
     })
 })
 
+const userApi = createApi({
+    reducerPath: 'userApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api' }),
+    tagTypes: ['Users'],
+    endpoints: (builder) => ({
+        setUser: builder.query({
+            query: (token) => ({
+                url: '/users/me',
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+            }),
+            providesTagsTags: 'User'
+        }),
+
+        createUser: builder.mutation({
+            query: (body) => ({
+                url: '/users/register',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: body
+            }),
+            invalidatesTags: 'User'
+        }),
+        
+        loginUser: builder.mutation({
+            query: (body) => ({
+                url: '/users/login',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: body
+            }),
+            invalidatesTags: 'User'
+        })
+    })
+})
+
 export const store = configureStore({
     reducer: {
-        booksApi: booksApi.reducer
+        booksApi: booksApi.reducer,
+        userApi: userApi.reducer
     },
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(booksApi.middleware)
+        getDefaultMiddleware().concat(booksApi.middleware, userApi.middleware)
 })
 
 export const { useGetAllBooksQuery, useGetBookByIdQuery } = booksApi
+export const { useSetUserQuery, useCreateUserMutation, useLoginUserMutation } = userApi
